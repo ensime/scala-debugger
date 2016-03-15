e1d38cb4052fe44d59417e2fde8f209f4e29b58c
Chip Senkbeil <chip.senkbeil@gmail.com>
2016-03-14 22:35:21 -0500

    Updated parallel execution for testing to common, fixed undefined issue

--- scala-debugger-language/src/main/scala/org/scaladebugger/language/interpreters/DebuggerInterpreter.scala
+++ scala-debugger-language/src/main/scala/org/scaladebugger/language/interpreters/DebuggerInterpreter.scala
@@ -45,7 +45,9 @@ class DebuggerInterpreter(
       parameters.map(models.Identifier.apply),
       (m: Map[models.Identifier, models.Expression], _) => toExpression(
         function(m.filter {
-          case (i, e) => e != models.Undefined
+          // TODO: Support filtering for native functions so you don't have
+          //       variables with an undefined model as the value
+          case (i, e) => true//e != models.Undefined
         }.map { case (i, e) => (i.name, toPrimitiveValue(e).get) })
       ).get,
       Option(documentation)
