f6a4c3ad49b3c6355bc3c73dbe83232c6dea7293
Chip Senkbeil <chip.senkbeil@gmail.com>
2016-11-24 11:05:37 -0600

    Added docs to builtin debugger language functions

--- scala-debugger-language/src/main/scala/org/scaladebugger/language/interpreters/DebuggerInterpreterBuiltins.scala
+++ scala-debugger-language/src/main/scala/org/scaladebugger/language/interpreters/DebuggerInterpreterBuiltins.scala
@@ -10,22 +10,38 @@ import scala.util.{Failure, Success, Try}
  * Contains built-in functions for the debugger interpreter.
  */
 trait DebuggerInterpreterBuiltins { this: DebuggerInterpreter =>
+  /** Mark parameter as required. */
+  @inline private def R(name: String, doc: String = "") =
+  (name, "Required" + (if (doc.nonEmpty) " ~ " + doc else ""))
+
+  /** Mark parameter as optional. */
+  @inline private def O(name: String, doc: String = "") =
+  (name, "Optional" + (if (doc.nonEmpty) " ~ " + doc else ""))
+
   // Interpreting
+  private lazy val evalCodeIdentifier = {
+    val (name, doc) = R("code", "text to be parsed and evaluated")
+    models.Identifier(name, Some(doc))
+  }
   this.bindFunctionExpression("eval", models.NativeFunction(
-    Seq(models.Identifier("code")),
+    Seq(evalCodeIdentifier),
     (m, s) => toExpression(interpret(toBaseValue(m.getOrElse(
-      models.Identifier("code"),
+      evalCodeIdentifier,
       models.Undefined.toScalaValue
     ).asInstanceOf[models.Expression], s).toScalaValue.toString, s).get).get,
     Some("""Evaluate the text as a local code snippet.""")
   ))
 
   // Debugging
+  private lazy val parseCodeIdentifier = {
+    val (name, doc) = R("code", "text to be parsed")
+    models.Identifier(name, Some(doc))
+  }
   this.bindFunctionExpression("parse", models.NativeFunction(
-    Seq(models.Identifier("code")),
+    Seq(parseCodeIdentifier),
     (m, s) => {
       val code = toBaseValue(m.getOrElse(
-        models.Identifier("code"),
+        parseCodeIdentifier,
         models.Undefined
       )).toScalaValue.toString
 
@@ -37,26 +53,66 @@ trait DebuggerInterpreterBuiltins { this: DebuggerInterpreter =>
   ))
 
   // Side effects
-  this.bindFunction("print", Seq("text"), DefaultFunctions.Print(out),
-    """Prints to standard out. Invoke using print("some text").""")
-  this.bindFunction("printErr", Seq("text"), DefaultFunctions.Print(err),
-    """Prints to standard error. Invoke using printErr("some text").""")
+  this.bindFunctionWithParamDocs(
+    "print", Seq(R("text")), DefaultFunctions.Print(out),
+    """Prints to standard out. Invoke using print("some text")."""
+  )
+  this.bindFunctionWithParamDocs(
+    "printErr", Seq(R("text")), DefaultFunctions.Print(err),
+    """Prints to standard error. Invoke using printErr("some text")."""
+  )
 
   // Mathematical operators
-  this.bindFunction("plusPlus", Seq("l", "r"), DefaultFunctions.PlusPlus)
-  this.bindFunction("plus", Seq("l", "r"), DefaultFunctions.Plus)
-  this.bindFunction("minus", Seq("l", "r"), DefaultFunctions.Minus)
-  this.bindFunction("multiply", Seq("l", "r"), DefaultFunctions.Multiply)
-  this.bindFunction("divide", Seq("l", "r"), DefaultFunctions.Divide)
-  this.bindFunction("modulus", Seq("l", "r"), DefaultFunctions.Modulus)
+  this.bindFunctionWithParamDocs(
+    "plusPlus", Seq(R("l"), R("r")), DefaultFunctions.PlusPlus,
+    """Equivalent to l ++ r."""
+  )
+  this.bindFunctionWithParamDocs(
+    "plus", Seq(R("l"), R("r")), DefaultFunctions.Plus,
+    """Equivalent to l + r."""
+  )
+  this.bindFunctionWithParamDocs(
+    "minus", Seq(R("l"), R("r")), DefaultFunctions.Minus,
+    """Equivalent to l - r."""
+  )
+  this.bindFunctionWithParamDocs(
+    "multiply", Seq(R("l"), R("r")), DefaultFunctions.Multiply,
+    """Equivalent to l * r."""
+  )
+  this.bindFunctionWithParamDocs(
+    "divide", Seq(R("l"), R("r")), DefaultFunctions.Divide,
+    """Equivalent to l / r."""
+  )
+  this.bindFunctionWithParamDocs(
+    "modulus", Seq(R("l"), R("r")), DefaultFunctions.Modulus,
+    """Equivalent to l % r."""
+  )
 
   // Logical operators
-  this.bindFunction("lessThan", Seq("l", "r"), DefaultFunctions.LessThan)
-  this.bindFunction("lessThanEqual", Seq("l", "r"), DefaultFunctions.LessThanEqual)
-  this.bindFunction("greaterThan", Seq("l", "r"), DefaultFunctions.GreaterThan)
-  this.bindFunction("greaterThanEqual", Seq("l", "r"), DefaultFunctions.GreaterThanEqual)
-  this.bindFunction("equal", Seq("l", "r"), DefaultFunctions.Equal)
-  this.bindFunction("notEqual", Seq("l", "r"), DefaultFunctions.NotEqual)
+  this.bindFunctionWithParamDocs(
+    "lessThan", Seq(R("l"), R("r")), DefaultFunctions.LessThan,
+    """Equivalent to l < r."""
+  )
+  this.bindFunctionWithParamDocs(
+    "lessThanEqual", Seq(R("l"), R("r")), DefaultFunctions.LessThanEqual,
+    """Equivalent to l <= r."""
+  )
+  this.bindFunctionWithParamDocs(
+    "greaterThan", Seq(R("l"), R("r")), DefaultFunctions.GreaterThan,
+    """Equivalent to l > r."""
+  )
+  this.bindFunctionWithParamDocs(
+    "greaterThanEqual", Seq(R("l"), R("r")), DefaultFunctions.GreaterThanEqual,
+    """Equivalent to l >= r."""
+  )
+  this.bindFunctionWithParamDocs(
+    "equal", Seq(R("l"), R("r")), DefaultFunctions.Equal,
+    """Equivalent to l == r."""
+  )
+  this.bindFunctionWithParamDocs(
+    "notEqual", Seq(R("l"), R("r")), DefaultFunctions.NotEqual,
+    """Equivalent to l != r."""
+  )
 
   private def parseResultsToString(results: Try[Seq[AnyRef]], input: String): String = results match {
     case Success(r)               => r.mkString(",")
