e5962c99955f584e367028067c5eb29b45c2e1a8
Chip Senkbeil <chip.senkbeil@gmail.com>
2016-12-05 20:21:02 -0600

    Updated Scala 2.12.0 to 2.12.1

--- project/Common.scala
+++ project/Common.scala
@@ -24,7 +24,7 @@ object Common {
     // Default version when not cross-compiling
     scalaVersion := "2.10.6",
 
-    crossScalaVersions := Seq("2.10.6", "2.11.8", "2.12.0"),
+    crossScalaVersions := Seq("2.10.6", "2.11.8", "2.12.1"),
 
     scalacOptions ++= Seq(
       "-encoding", "UTF-8",
