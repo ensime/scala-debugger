f2c63827780b241b92569ea79c482949ad8818cb
Chip Senkbeil <chip.senkbeil@gmail.com>
2017-07-07 15:13:11 -0500

    Updated to use modern page interface

--- scala-debugger-docs/src/main/scala/org/scaladebugger/docs/layouts/SitePage.scala
+++ scala-debugger-docs/src/main/scala/org/scaladebugger/docs/layouts/SitePage.scala
@@ -13,54 +13,44 @@ import org.scaladebugger.docs.styles.Implicits._
 /**
  * Represents the layout for a common site page.
  *
- * @param preHeadContent Content to be added at the beginning of the <head>
- * @param postHeadContent Content to be added at the end of the <head>
- * @param preBodyContent Content to be added at the beginning of the <body>
- * @param postBodyContent Content to be added at the end of the <body>
- * @param htmlModifiers Modifiers to apply on the <html> tag
- * @param bodyModifiers Modifiers to apply on the <body> tag
  * @param selectedMenuItems Will mark each menu item whose name is provided
  *                          as selected
  * @param syntaxHighlightTheme The theme to use for syntax highlighting; themes
  *                             are from the highlight.js list
  */
 abstract class SitePage(
-  override val preHeadContent: (Context) => Seq[Modifier] = _ => Nil,
-  override val postHeadContent: (Context) => Seq[Modifier] = _ => Nil,
-  override val preBodyContent: (Context) => Seq[Modifier] = _ => Nil,
-  override val postBodyContent: (Context) => Seq[Modifier] = _ => Nil,
-  override val htmlModifiers: (Context) => Seq[Modifier] = _ => Nil,
-  override val bodyModifiers: (Context) => Seq[Modifier] = _ => Nil,
   val selectedMenuItems: Seq[String] = Nil,
   val syntaxHighlightTheme: String = "agate"
-) extends Page(
-  preHeadContent = c => preHeadContent(c) ++ Seq(
-    FontAwesomeCSS(),
-    HighlightCSS(theme = syntaxHighlightTheme),
-    PageStyle.styleSheetText.toStyleTag,
-    TopbarNavStyle.styleSheetText.toStyleTag,
-    TabsStyle.styleSheetText.toStyleTag
-  ),
-  postHeadContent = c => postHeadContent(c),
-  preBodyContent = c => preBodyContent(c) ++ Seq(
-    Header(
-      MainMenuBar(
-        MainMenuLogo(),
-        MainMenu(c.mainMenuItems: _*)
+) extends Page {
+  override protected def preHeadContent(context: Context): Seq[Modifier] =
+    super.preHeadContent(context) ++ Seq(
+      FontAwesomeCSS(),
+      HighlightCSS(theme = syntaxHighlightTheme),
+      PageStyle.styleSheetText.toStyleTag,
+      TopbarNavStyle.styleSheetText.toStyleTag,
+      TabsStyle.styleSheetText.toStyleTag
+    )
+
+  override protected def preBodyContent(context: Context): Seq[Modifier] =
+    super.preBodyContent(context) ++ Seq(
+      Header(
+        MainMenuBar(
+          MainMenuLogo(),
+          MainMenu(context.mainMenuItems: _*)
+        )
       )
     )
-  ),
-  postBodyContent = c => Seq(
-    Footer(
-      authorName = "Chip Senkbeil",
-      authorUrl = new URL("https://chipsenkbeil.com/"),
-      startYear = 2015
-    ),
-    ClipboardJS(),
-    HighlightJS(),
-    ClipboardJSInit(),
-    HighlightJSInit()
-  ) ++ postBodyContent(c),
-  htmlModifiers = c => htmlModifiers(c),
-  bodyModifiers = c => bodyModifiers(c)
-)
+
+  override protected def postBodyContent(context: Context): Seq[Modifier] =
+    Seq(
+      Footer(
+        authorName = "Chip Senkbeil",
+        authorUrl = new URL("https://chipsenkbeil.com/"),
+        startYear = 2015
+      ),
+      ClipboardJS(),
+      HighlightJS(),
+      ClipboardJSInit(),
+      HighlightJSInit()
+    ) ++ super.postBodyContent(context)
+}
