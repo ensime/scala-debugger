ae249348f2ce4f65ee6409f5c9e166252bde387a
Chip Senkbeil <chip.senkbeil@gmail.com>
2017-08-04 23:10:27 -0700

    Updated to 0.1.1 of site-generator

--- project/Docs.scala
+++ project/Docs.scala
@@ -5,7 +5,7 @@ object Docs {
   /** Docs-specific project settings. */
   val settings = Seq(
     libraryDependencies ++= Seq(
-      "org.senkbeil" %% "site-generator" % "0.1.0"
+      "org.senkbeil" %% "site-generator" % "0.1.1"
     )
   )
 }
